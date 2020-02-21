f7d406f7e5968187afb7685b542d67b709ba6261
OfirTheOne <ofir3322@gmail.com>
2020-02-21 17:15:12 +0200

    add build publish script

---
+++ build-publish.job.sh
@@ -0,0 +1,10 @@
+echo "Clean current build"
+rm -rf ./dist-prod
+
+echo "Build process"
+npm run build-prod
+cp package.json ./dist-prod/package.json
+cp README.md    ./dist-prod/README.md
+
+echo "Publish process"
+npm publish ./dist-prod
