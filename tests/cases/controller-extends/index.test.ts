ed22d1db97291a797419adda155978cf5f848246
Ofir Genish <ofir3322@gmail.com>
2020-05-09 20:20:23 +0300

    test description

--- tests/cases/controller-extends/index.test.ts
+++ tests/cases/controller-extends/index.test.ts
@@ -5,21 +5,41 @@ import * as request from 'supertest';
 
 const { app } = new App(3000);
 
-request(app).post(
-    '/api/feature/product/',
-).end(
-    (err, req) => {
-        expect(err).to.be.null
-    }
-)
 
-request(app).get(
-    '/api/feature/product/',
-).end(
-    (err, req) => {
-        expect(err).to.be.null
-        expect(req.body).to.have.property('name')
-        expect(req.body.name).to.be.eq("product A")
 
-    }
-)
+
+describe("Feature : Extends Controllers", function() {
+    it("test sub controller api call", function(done) {
+        request(app).post(
+            '/api/feature/product/',
+        ).send({
+            name: "product A"
+        }).end(
+            (err, req) => {
+                expect(err).to.be.null
+                expect(req.body).to.have.property('name')
+                expect(req.body.name).to.be.eq("product A")
+                done(err)
+        
+            }
+        )
+    
+    })
+    
+    
+    it("test extended (parent controller) api call", function(done) {
+        request(app).get(
+            '/api/feature/product/',
+        ).end(
+            (err, req) => {
+                expect(err).to.be.null
+                expect(req.body).to.have.property('name')
+                expect(req.body.name).to.be.eq("product A")
+                done(err)
+        
+            }
+        )
+    
+    })
+
+})
